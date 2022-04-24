35268a967d78a278a27382100f1e817fdd6ef9da
Sadeeed <sadeeedw@gmail.com>
2022-04-24 13:15:59 +0500

    :zap: add db2csv view

--- mqtt/views.py
+++ mqtt/views.py
@@ -17,3 +17,28 @@ class IndexView(LoginRequiredMixin, TemplateView):
         context["Z"] = CollarData.objects.filter(topic='collar/accel/Z').last()
         context["bpm"] = CollarData.objects.filter(topic='collar/bpm').last()
         return self.render_to_response(context)
+
+
+class dbtoCSVView(TemplateView):
+    template_name = 'temp.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        temperature = CollarData.objects.filter(topic='collar/temperature').all()
+        bellowing = CollarData.objects.filter(topic='collar/bellowing').all()
+        x = CollarData.objects.filter(topic='collar/accel/X').all()
+        y = CollarData.objects.filter(topic='collar/accel/Y').all()
+        z = CollarData.objects.filter(topic='collar/accel/Z').all()
+        bpm = CollarData.objects.filter(topic='collar/bpm').all()
+        gas = CollarData.objects.filter(topic='collar/ketosis').all()
+        leg = CollarData.objects.filter(topic='leg/mvmt/').all()
+
+        count = 1
+
+        with open(f'dataset-{datetime.today().date()}.csv', 'w') as csvfile:
+            csvfile = csv.writer(csvfile)
+            csvfile.writerow(['#', 'temperature', 'bellowing', 'x', 'y', 'z', 'bpm', 'gas', 'leg'])
+            for t, b, x, y, z, bp, g, l in zip(temperature, bellowing, x, y, z, bpm, gas, leg):
+                csvfile.writerow([count, t.message, b.message, x.message, y.message, z.message, bp.message, g.message, l.message])
+                count += 1
+        return self.render_to_response(context)
