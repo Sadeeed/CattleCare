47da8da92200d05d10c8ffd3f0280fb9f0e01807
Sadeeed <sadeeedw@gmail.com>
2022-04-25 14:31:09 +0500

    :zap: update urls

--- mqtt/urls.py
+++ mqtt/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from mqtt.views import IndexView
+from mqtt.views import IndexView, dbtoCSVView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('csv/', dbtoCSVView.as_view(), name='csv'),
 ]
